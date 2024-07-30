daa5e7aa9e384d1b5fafa40db15e111f5f71232a
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2024-07-30 12:02:48 +0100

    Update run.py

--- run.py
+++ run.py
@@ -300,6 +300,9 @@ netcdf_path = os.path.join(run_path, 'R1C1_SurfaceMaps.nc')
 
 output.to_geotiff(os.path.join(surface_maps, 'R1_C1_max_depth.csv'), geotiff_path, srid=27700)
 
+print('permeable_areas:',type(permeable_areas))
+print('permeable_areas:',type(str(permeable_areas)))
+            
 output.to_netcdf(surface_maps, out_path=netcdf_path, srid=projection,
                  attributes=dict(
                     rainfall_mode=rainfall_mode,
